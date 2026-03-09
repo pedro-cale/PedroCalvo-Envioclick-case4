8d48e7128ca8caadfc204af90ac6fa7abca6ed1c
Pedro Calvo Leon <pedrocale96@gmail.com>
2026-03-08 20:50:31 -0600

    Lambda Terraform creation

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,13 +1,13 @@
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-west-2"
+    description = "AWS region"
+    type        = string
+    default     = "us-west-2"
 }
 
 variable "function_name" {
-  description = "Name of the Lambda function"
-  type        = string
-  default     = "case4-lambda"
+    description = "Name of the Lambda function"
+    type        = string
+    default     = "case4-lambda"
 }
 
 variable "aws_account" {
